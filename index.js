1b9f74c999d68457f941a9e3972411e7d8162c0f
lautaro <you@example.com>
2023-11-06 17:14:11 -0300

    First commit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
+const http = require("node:http")
 const cors = require('cors')
 
 const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  console.log("asdasdsadasdasdas")
-  res.json({
-    msj: 'Hola desde FL0'
-  })
+app.get('/data', async (req, res) => {
+  const { url } = req.query
+
+  try {
+    fetch(url)
+      .then(j => j.text())
+      .then(data => {
+        const title = data.match(/<title>(.*?)<\/title>/i)
+        const favicon = data.match(/<link rel="icon" href="(.*?)"/i)
+
+        res.json({
+          title: title[1],
+          favicon: favicon[1]
+        })
+
+      })
+
+  } catch (error) {
+
+  }
+
+
 })
 
 
